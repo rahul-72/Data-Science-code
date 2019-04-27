0a228d62f9cd23af74fc7c6921f47faf4a9b4cba
rahul-72 <charan7rahul@gmail.com>
2019-04-27 10:29:24 +0530

    changes in frequency code

--- data_science/frequency.py
+++ data_science/frequency.py
@@ -5,36 +5,40 @@
 
 
 import numpy as np
-n,l,h = map(int, input("Enter number of itmes,low value,high value:  ").split(','))
-data = [np.random.randint(l,h) for var in range(n)]
-k = 1 + 3.322*(np.math.log10(n))
-c = (h-l)/k
+try:
+    n,l,h = map(int, input("Enter number of itmes,low value,high value:  ").split(','))
+    data = [np.random.randint(l,h) for var in range(n)]
+    k = 1 + 3.322*(np.math.log10(n))
+    c = (h-l)/k
 
-print()
-print()
-lcf = 0
-mcf = n  
+    print()
+    print()
+    lcf = 0
+    mcf = n  
 
-classes = []
-for i in range(round(k)):
-    cls = (round(l), round(l+c))
-    count = 0
-    for var in range(round(l), round(l+c)):      
-        count+=data.count(var)
-    lcf+=count
-    classes.append([cls,count,lcf,mcf])
-    mcf-=count
-    l = l+c
+    classes = []
+    for i in range(round(k)):
+        cls = (round(l), round(l+c))
+        count = 0
+        for var in range(round(l), round(l+c)):      
+            count+=data.count(var)
+        lcf+=count
+        classes.append([cls,count,lcf,mcf])
+        mcf-=count
+        l = l+c
 
-print("Classes \t Frequency \t Less then Cummulative frequency \t More then cummulative frequency") 
-print()
-t = 0
-for key,value,qwe,qaz in classes:
-    print(key,'\t',value,'\t\t\t\t',qwe,'\t\t\t\t\t', qaz)
-    t+=value
-print()  
-print()
-print("Total number of itmes \t", t)  
+    print("Classes \t Frequency \t Less then Cummulative frequency \t More then cummulative frequency") 
+    print()
+    t = 0
+    for key,value,qwe,qaz in classes:
+        print(key,'\t',value,'\t\t\t\t',qwe,'\t\t\t\t\t', qaz)
+        t+=value
+    print()  
+    print()
+    print("Total number of itmes \t", t)  
+    
+except Exception as e:
+    print("Error--->>>>   Number of items, lower value, upper value must be seperated by comma(,)")
 
 
 # 
